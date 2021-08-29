a216877c211bbf6d385505de6ee081ed7244e6ae
Sergei Domnenkov <SADomnenkov@sberbank.ru>
2021-08-29 22:20:55 +0300

    develop | cerbers_deploy.groovy: FIX #15

--- cerbers_deploy.groovy
+++ cerbers_deploy.groovy
@@ -12,14 +12,7 @@ node("magicBook") {
                       url: 'https://github.com/mrazotto/devops.git']]])
     }
     stage('Download .zip') {
-        checkout([$class: 'GitSCM',
-                  branches: [[name: "*/develop"]],
-                  doGenerateSubmoduleConfigurations: false,
-                  gitTool: 'Default',
-                  submoduleCfg: [],
-                  userRemoteConfigs: [[
-                      credentialsId: "mrazotto",
-                      url: 'https://github.com/mrazotto/nydus.git']]])
+        sh 'git clone https://github.com/mrazotto/nydus.git'
     }
     stage('Deploy') {
         sh """ansible-playbook playbook.yml"""
