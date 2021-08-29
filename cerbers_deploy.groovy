01afa8df7b6be575d3d67f37a39e6a87b4755ffe
Sergei Domnenkov <SADomnenkov@sberbank.ru>
2021-08-29 17:31:06 +0300

    develop | cerbers_deploy.groovy: FIX #2

--- cerbers_deploy.groovy
+++ cerbers_deploy.groovy
@@ -1,6 +1,16 @@
 import hudson.Util;
 
 node("magicBook") {
+    stage('Clone pipeline') {
+        checkout([$class: 'Git',
+                  branches: [[name: "*/develop"]],
+                  doGenerateSubmoduleConfigurations: false,
+                  gitTool: 'Default',
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[
+                      credentialsId: "mrazotto",
+                      url: 'https://github.com/mrazotto/devops.git']]])
+    }
     stage('Deploy') {
         sh """ansible-playbook playbook.yml"""
     }
