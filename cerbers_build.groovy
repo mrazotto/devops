aba965d3d9442212b4bc5c7be7da1d18e60894fd
Sergei Domnenkov <SADomnenkov@sberbank.ru>
2021-09-09 19:54:17 +0300

    develop | + cerbers build

---
+++ cerbers_build.groovy
@@ -0,0 +1,29 @@
+import hudson.Util;
+
+node("magicBook") {
+    stage('Git Clone') {
+            checkout([$class            : 'GitSCM',
+                      branches          : [[name: "*/develop"]],
+                      doGenerateSubmoduleConfigurations: false,
+                      gitTool           : 'Default',
+                      submoduleCfg      : [],
+                      userRemoteConfigs : [[
+                          credentialsId : "mrazotto",
+                          url           : 'https://github.com/mrazotto/cerbers.git']]])
+
+        dir('sberrec_devops') {
+            checkout([$class            : 'GitSCM',
+                      branches          : [[name: "*/develop"]],
+                      gitTool           : 'Default',
+                      userRemoteConfigs : [[
+                          credentialsId : "mrazotto",
+                          url           : 'https://github.com/mrazotto/devops.git']]])
+        }
+    }
+    stage('Build distrib') {
+        sh """zip -r cerbers.zip \
+                ./aserver/config/application.properties \
+                ./blserver/config/application* \
+                ./restserver/config/application.properties"""
+    }
+}
---
+++ scripts/service/cerbers_templates.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+blserver_config="blserver/config/application.properties"
+
+propName=""
+propValue=""
+propAnsName=""
+
+for item in $(cat $blserver_config | tr -d ' ')
+do
+    if [[ $item == *"="* ]] && [[ $item != *"#"* ]]
+    then
+        propName=`echo $item | cut -d '=' -f 1`
+        propValue=`echo $item | cut -d '=' -f 2`
+        propAnsName=`echo $propName | sed 's/[-|.]/_/g'`
+
+        echo $propName | { tr -d '\n' ; echo "={% if $propAnsName is defined %}{{$propAnsName}}{% else %}$propValue{% endif %}"; } \
+            >> ./cerbers_templates/blserver/config/application.properties.j2
+    fi
+done
