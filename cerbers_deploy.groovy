a554e3dd6117da204dee63b719810861ed1b2718
Sergei Domnenkov <SADomnenkov@sberbank.ru>
2021-09-15 13:23:05 +0300

    develop | FAILURE/UNSTABLE checks...

--- cerbers_deploy.groovy
+++ cerbers_deploy.groovy
@@ -20,7 +20,7 @@ node("magicBook") {
         if (skipRemainingStages) {
             echo "4e kavo!"
             currentBuild.result = 'FAILURE'
-            stageResult: 'FAILURE'
+            sh('false')
             result = 'FAILURE';
         }
         sh """ansible-playbook playbook.yml"""
