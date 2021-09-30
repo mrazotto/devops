8366352616099dd97273dffd1935dd23e6a0fdb2
Sergei Domnenkov <SADomnenkov@sberbank.ru>
2021-09-30 11:44:11 +0300

    develop | update

--- cerbers_deploy.groovy
+++ cerbers_deploy.groovy
@@ -19,7 +19,7 @@ node("magicBook") {
     stage('Deploy') {
         if (skipRemainingStages) {
             echo "CHECKING FAILED..."
-            result = "FAIL"
+            sh "exit 1"
         }
         sh """ansible-playbook playbook.yml"""
     }
